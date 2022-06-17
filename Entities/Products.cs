321d17cdd5351248b54ed6463a620e7346284481
Gabriel Vazarini <camaravazarini@gmail.com>
2022-06-16 21:43:09 -0300

    RestricaoGenerics

---
+++ Entities/Products.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace RestricaoGenerics.Entities
+{
+    internal class Products : IComparable 
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public Products(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+        public override string ToString()
+        {
+            return Name + ", " + Price.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public int CompareTo(object? obj)
+        {
+            if(!(obj is Products))
+            {
+                throw new ArgumentException("Arguments are not comparable!");
+            }
+            Products other  = obj as Products;
+            return Price.CompareTo(other.Price);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using  System.Collections.Generic;
+using RestricaoGenerics.Services;
+using RestricaoGenerics.Entities;
+using System.Globalization;
+namespace RestricaoGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Products> list = new List<Products>();
+            Console.WriteLine("Enter with the Products: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter with Name and Price (Phone,1000.00): ");
+            for (int i = 0; i < n; i++)
+            {
+              string[] vect = Console.ReadLine().Split(',');
+                string name = vect[0];
+                double price = double.Parse(vect[1], CultureInfo.InvariantCulture);
+                list.Add(new Products(name, price));
+            }
+            CalculatorService calculatorService = new CalculatorService();
+
+            Products max = calculatorService.Max(list);
+            Console.WriteLine("Max: ");
+            Console.WriteLine(max);
+        }
+    }
+}
